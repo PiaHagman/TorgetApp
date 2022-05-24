73f006c33b4b8610de168eb68fb80bdcbbfb51fc
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-05-24 14:10:07 +0200

    Added a database context To get the database context to work I had to create some new models for Bild, Tag and Bevakning. This due to List<string> not working. Added the Identity package in program as well.

--- Torget (Blocket klon)/Data/Models/Annons.cs
+++ Torget (Blocket klon)/Data/Models/Annons.cs
@@ -1,22 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Torget__Blocket_klon_.Data.Models;
 
 public class Annons
 {
-    [Required]
-    public string Titel { get; set; }
-    [Required]
-    public string Kategori { get; set; }
-    [Required]
-    public string Beskrivning { get; set; }
-    [Required]
-    public int Pris { get; set; }
+    public int Id { get; set; }
+    [Required] public string Titel { get; set; }
+    [Required] public string Kategori { get; set; }
+    [Required] public string Beskrivning { get; set; }
+    [Required] public int Pris { get; set; }
 
     public DateTime DatumUpplagd { get; } = DateTime.Now;
     public DateTime DatumUppdaterad { get; set; }
-    [Required]
-    public TorgetUser TorgetUser { get; set; }
-    public List<string> Bilder { get; set; }
-    public List<string> Taggar { get; set; }
+
+    [InverseProperty("Annonser")] public TorgetUser TorgetUser { get; set; }
+
+    [InverseProperty("SparadeAnnonser")] public List<TorgetUser>? UsersSomSparatAnnons { get; set; }
+    public List<Bild> Bilder { get; set; }
+    public List<Tag> Taggar { get; set; }
 }
--- Torget (Blocket klon)/Program.cs
+++ Torget (Blocket klon)/Program.cs
@@ -1,6 +1,58 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Torget__Blocket_klon_.Data.Models;
+using Torget__Blocket_klon_.Data.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<TorgetDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
+
+#region Identity
+
+builder.Services.AddDefaultIdentity<TorgetUser>(options =>
+        options.User.RequireUniqueEmail = true)
+    .AddEntityFrameworkStores<TorgetDbContext>();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    //TODO: Fixa rätt paths
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30); //TODO: Hur länge vill vi ha?
+    options.LoginPath = "/User/Login";
+    options.SlidingExpiration = true;
+    options.ReturnUrlParameter = "ReturnUrl";
+    options.AccessDeniedPath = "/AccessDenied";
+});
+
+#endregion
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// när vi är i "debug" läge
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var ctx = services.GetRequiredService<TorgetDbContext>();
+    app.UseDeveloperExceptionPage();
+
+    await ctx.Database.EnsureDeletedAsync();
+    await ctx.Database.EnsureCreatedAsync();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
