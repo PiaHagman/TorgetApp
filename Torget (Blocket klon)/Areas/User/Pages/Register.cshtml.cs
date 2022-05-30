63d213aeb899a4f3a7e51e7e211640528aa42777
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-05-30 17:50:32 +0200

    Changed to a variable for background color of buttons in index.css. Worked on the style of Register Page.
    #16

--- Torget (Blocket klon)/Areas/User/Pages/Register.cshtml.cs
+++ Torget (Blocket klon)/Areas/User/Pages/Register.cshtml.cs
@@ -4,56 +4,55 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Torget__Blocket_klon_.Data.Models;
 
-namespace Torget__Blocket_klon_.Areas.User.Pages
+namespace Torget__Blocket_klon_.Areas.User.Pages;
+
+public class RegisterModel : PageModel
 {
-    public class RegisterModel : PageModel
+    private readonly UserManager<TorgetUser> _userManager;
+    private readonly ILogger<RegisterModel> _logger;
+
+    [Required(ErrorMessage = "Du måste ange en email")]
+    [EmailAddress]
+    [BindProperty]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Du måste ange ett telefon nummer")]
+    [BindProperty]
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "Telefon nummer")]
+    public string PhoneNumber { get; set; }
+
+    [Required(ErrorMessage = "Du måste ange en postkod")]
+    [BindProperty]
+    [DataType(DataType.PostalCode)]
+    [Display(Name = "Postnummer")]
+    public string ZipCode { get; set; }
+
+    [Required(ErrorMessage = "Du måste ange ett lösenord")]
+    [BindProperty]
+    [DataType(DataType.Password)]
+    [Display(Name = "Lösenord")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Konfirmation av lösenord är ett måste")]
+    [BindProperty]
+    [DataType(DataType.Password)]
+    [Display(Name = "Upprepa lösenord")]
+    [Compare("Password", ErrorMessage = "Lösenorden matchar inte.")]
+    public string ConfirmPassword { get; set; }
+
+    public RegisterModel(UserManager<TorgetUser> userManager, ILogger<RegisterModel> logger)
     {
-        private readonly UserManager<TorgetUser> _userManager;
-        private readonly ILogger<RegisterModel> _logger;
+        _userManager = userManager;
+        _logger = logger;
+    }
 
-        [Required]
-        [EmailAddress]
-        [BindProperty]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+    public void OnGet()
+    {
+    }
 
-        [Required]
-        [BindProperty]
-        [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Telefon nummer")]
-        public string PhoneNumber { get; set; }
-
-        [Required]
-        [BindProperty]
-        [DataType(DataType.PostalCode)]
-        [Display(Name = "Postnummer")]
-        public string ZipCode { get; set; }
-
-        [Required(ErrorMessage = "Du måste ange ett lösenord")]
-        [BindProperty]
-        [DataType(DataType.Password)]
-        [Display(Name = "Lösenord")]
-        public string Password { get; set; }
-
-        [Required(ErrorMessage = "Konfirmation av lösenord är ett måste")]
-        [BindProperty]
-        [DataType(DataType.Password)]
-        [Display(Name = "Upprepa lösenord")]
-        [Compare("Password", ErrorMessage = "Lösenorden matchar inte.")]
-        public string ConfirmPassword { get; set; }
-
-        public RegisterModel(UserManager<TorgetUser> userManager, ILogger<RegisterModel> logger)
-        {
-            _userManager = userManager;
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
-
-        public async Task OnPost()
-        {
-        }
+    public async Task OnPost()
+    {
     }
 }
