462a7e1a8b06bdea9a2873a4e10ed89c6b49243d
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-05-30 10:39:59 +0200

    Add: skapaAnnons Page

---
+++ Torget (Blocket klon)/Areas/Konto/Pages/SkapaAnnons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Torget__Blocket_klon_.Areas.Konto.Pages
+{
+    public class SkapaAnnonsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
