bb518d2bc48ee8455f3e8b2ada6ab11efc0436b7
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-05-30 12:07:04 +0200

    Add: ViewImports SkapaAnnons started

--- Torget (Blocket klon)/Areas/Konto/Pages/SkapaAnnons.cshtml.cs
+++ Torget (Blocket klon)/Areas/Konto/Pages/SkapaAnnons.cshtml.cs
@@ -1,12 +1,25 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using Torget__Blocket_klon_.Data.Models;
+using Torget__Blocket_klon_.Data.Services;
 
 namespace Torget__Blocket_klon_.Areas.Konto.Pages
 {
 
     public class SkapaAnnonsModel : PageModel
     {
+
+        private readonly UserManager<TorgetUser> _userManager;
+        private readonly AdHandler _adHandler;
+
+
+        public SkapaAnnonsModel(UserManager<TorgetUser> userManager, AdHandler adHandler)
+        {
+            _userManager = userManager;
+            _adHandler = adHandler;
+        }
 
         [BindProperty]
         public InputModel Input { get; set; }
@@ -26,7 +39,7 @@ namespace Torget__Blocket_klon_.Areas.Konto.Pages
             [Required]
             [DataType(DataType.Currency)]
             [Display(Name = "Pris")]
-            public string Pris { get; set; }
+            public int Pris { get; set; }
 
             [Required]
             [DataType(DataType.Text)]
@@ -47,9 +60,28 @@ namespace Torget__Blocket_klon_.Areas.Konto.Pages
         {
         }
 
-        public Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
+            if (!ModelState.IsValid) return Page();
 
+            var user = await _userManager.FindByIdAsync(
+                "43eefa21 - 9b75 - 4926 - 9e1f - d9a878aa5f24"); //Tillfällig user.
+
+            var newTorgetAd = new TorgetAd
+            {
+                Title = Input.Titel,
+                Category = Input.Kategori,
+                Description = Input.Beskrivning,
+                Price = Input.Pris,
+                DatePosted = DateTime.Now,
+                TorgetUser = user,
+
+            };
+
+
+            await _adHandler.Create(newTorgetAd);
+
+            return Page(); //returna till en preview sida?
         }
     }
 }
