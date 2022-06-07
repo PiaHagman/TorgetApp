7abf12bc6c748c3a71116d0d82bc8e909fe8a62f
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-06-07 13:42:47 +0200

    Add: TorgetCategory model to project. Add: Seed Stakeholder choosen categories.

---
+++ Torget (Blocket klon)/Data/Models/TorgetCategory.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Torget__Blocket_klon_.Data.Models;
+
+public class TorgetCategory
+{
+    private string _name;
+
+    [Key] public string Name { get => _name; set => _name = value.ToUpper(); }
+}
